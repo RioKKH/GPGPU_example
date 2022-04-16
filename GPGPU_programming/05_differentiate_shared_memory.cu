b57685089722970fdc2a94a1d402059ace13a291
RioKKH <ryoichikakehi@gmail.com>
2022-04-16 20:58:21 +0900

    renamed:    06_differentiate.cu -> 05_differentiate.cu 	new file:   05_differentiate_constant_memory.cu 	new file:   05_differentiate_shared_memory.cu 	modified:   06_vectoradd.cu 	modified:   Makefile

---
+++ GPGPU_programming/05_differentiate_shared_memory.cu
@@ -0,0 +1,108 @@
+#include <iostream>
+#include <stdlib.h>
+#include <math.h>
+
+#define Lx (2.0 * M_PI)
+#define Nx (1024 * 1024)
+#define dx (Lx / (Nx - 1))
+#define Nbytes (Nx * sizeof(double))
+#define NT (256)
+#define NB (Nx / NT)
+
+void init(double *u)
+{
+	int i;
+	for (i=0; i<Nx; ++i)
+	{
+		u[i] = sin(i * dx);
+	}
+}
+
+__global__ void differentiate(double *u, double *dudx)
+{
+	int i = blockIdx.x * blockDim.x + threadIdx.x;
+	// 共有メモリの宣言と代入
+	// 右と左の袖領域を追加して宣言
+	__shared__ double su[1 + NT + 1];
+	int tx = threadIdx.x + 1;
+
+	su[tx] = u[i];
+	__syncthreads();
+
+	// メモリの話をしているのか、スレッドの話をしているのかがこんがらがる
+
+	// 袖領域の処理
+	// 左のブロックの一番右端のグローバルメモリから1つ右隣のブロックの左端
+	// の共有メモリに値をコピーする
+	if (blockIdx.x > 0 		&& threadIdx.x == 0)
+	{
+		su[tx - 1] = u[i - 1];
+	}
+	// 袖領域の処理
+	// 1つ右のブロックの一番左端のグローバルメモリから左隣のブロクの
+	// 右端の共有メモリに値をコピーする
+	if (blockIdx.x < gridDim.x - 1	&& threadIdx.x == blockDim.x - 1)
+	{
+		su[tx + 1] = u[i + 1];
+	}
+	// 境界条件の処理
+	// blockIdx.x == 0の一番左端の共有メモリにアサインする値を作成する
+	if (blockIdx.x == 0		&& threadIdx.x == 0)
+	{
+		su[tx - 1] = 3.0*su[tx] - 3.0*su[tx + 1] + su[tx + 2];
+	}
+	// 境界条件の処理
+	// blockIdx.x == girdDim.x -1の一番右端の共有メモリにアサインする値を作成する
+	// つまり全体でも一番右端の共有メモリとなる
+	if (blockIdx.x == gridDim.x - 1	&& threadIdx.x == blockIdx.x - 1)
+	{
+		su[tx + 1] = 3.0*su[tx] - 3.0*su[tx - 1] + su[tx - 2];
+	}
+	__syncthreads();
+	// 共有メモリに値の読み込み完了
+
+	// 全スレッドが中心差分を計算
+	dudx[i] = (su[tx + 1] - su[tx - 1]) / (2.0 * dx); 
+}
+
+int main(void)
+{
+	double *host_u, *host_dudx;
+	double *u, *dudx;
+	float elapsed_time_ms = 0.0f; // 経過時間保存用
+
+	// イベントを取り扱う変数
+	cudaEvent_t start, end;
+	// イベントのクリエイト
+	cudaEventCreate(&start);
+	cudaEventCreate(&end);
+
+	host_u		= (double *)malloc(Nbytes);
+	host_dudx	= (double *)malloc(Nbytes);
+	cudaMalloc((void **)&u, Nbytes);
+	cudaMalloc((void **)&dudx, Nbytes);
+
+	init(host_u);
+	cudaMemcpy(u, host_u, Nbytes, cudaMemcpyHostToDevice);
+
+	cudaEventRecord(start, 0);
+	differentiate<<<NB, NT>>>(u, dudx);
+	cudaEventRecord(end, 0);
+	cudaEventSynchronize(end);
+
+	cudaEventElapsedTime(&elapsed_time_ms, start, end);
+	std::cout << "Elapsed Time: " << elapsed_time_ms << std::endl;
+
+	cudaMemcpy(host_dudx, dudx, Nbytes, cudaMemcpyDeviceToHost);
+
+	cudaEventDestroy(start);
+	cudaEventDestroy(end);
+	free(host_u);
+	free(host_dudx);
+	cudaFree(u);
+	cudaFree(dudx);
+
+	return 0;
+}
+
+
--- GPGPU_programming/06_vectoradd.cu
+++ GPGPU_programming/06_vectoradd.cu
@@ -1,3 +1,5 @@
+#include <iostream>
+
 #define N (8 * 1024) // 64kBに収める
 #define Nbytes (N * sizeof(float))
 #define NT (256)
@@ -21,13 +23,36 @@ __global__ void add(float *a, float *b, float *c)
 int main(void)
 {
 	float *a, *b, *c;
+	// 経過時間保存用
+	float elapsed_time_ms = 0.0f;
+	// イベントを取り扱う変数
+	cudaEvent_t start, end;
+
+	cudaEventCreate(&start);
+	cudaEventCreate(&end);
 
 	cudaMalloc((void **)&a, Nbytes);
 	cudaMalloc((void **)&b, Nbytes);
 	cudaMalloc((void **)&c, Nbytes);
 
 	init<<<NB, NT>>>(a, b, c);
+
+	// イベント発生時間の記録
+	cudaEventRecord(start, 0);
 	add<<<NB, NT>>>(a, b, c);
+	// イベント発生時間の記録
+	cudaEventRecord(end, 0);
+
+	// startとstopの時間を正しく記録し終わっている事を保証
+	cudaEventSynchronize(end);
+
+	// イベント間の時間差を計算
+	cudaEventElapsedTime(&elapsed_time_ms, start, end);
+	std::cout << "Elapsed time: " << elapsed_time_ms << std::endl;
+
+	// イベントの破棄
+	cudaEventDestroy(start);
+	cudaEventDestroy(end);
 	
 	return 0;
 }
