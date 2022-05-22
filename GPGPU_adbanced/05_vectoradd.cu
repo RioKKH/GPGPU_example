bdb45c09d5667011346d8c0c264ea595c51d3534
RioKKH <ryoichikakehi@gmail.com>
2022-05-22 18:46:12 +0900

    new file:   05_vectoradd.cu 	new file:   05_vectoradd_broadcast.cu 	new file:   05_vectoradd_constant.cu 	new file:   Makefile 	modified:   ../misc/Makefile

---
+++ GPGPU_adbanced/05_vectoradd.cu
@@ -0,0 +1,40 @@
+#include <cuda.h>
+
+#define N (8 * 1024) // 64kBに収める
+#define Nbytes (N * sizeof(float))
+#define NT (256)
+#define NB (N / NT)
+
+__global__ void init(float *a, float *b, float *c)
+{
+    int i = blockIdx.x * blockDim.x + threadIdx.x;
+
+    a[i] = 1.0;
+    b[i] = 2.0;
+    c[i] = 0.0;
+}
+
+__global__ void add(float *a, float *b, float *c)
+{
+    int i = blockIdx.x * blockDim.x + threadIdx.x;
+
+    c[i] = a[i] + b[i];
+}
+
+int main(void)
+{
+    float *a, *b, *c;
+
+    cudaMalloc((void **)&a, Nbytes);
+    cudaMalloc((void **)&b, Nbytes);
+    cudaMalloc((void **)&c, Nbytes);
+
+    init<<<NB, NT>>>(a, b, c);
+    add<<<NB, NT>>>(a, b, c);
+
+    cudaFree(a);
+    cudaFree(b);
+    cudaFree(c);
+
+    return 0;
+}
---
+++ GPGPU_adbanced/05_vectoradd_constant.cu
@@ -0,0 +1,49 @@
+#include <cstdio>
+#include <cstdlib>
+#include <cuda.h>
+
+#define N (8 * 1024) // 64kBに収める
+#define Nbytes (N * sizeof(float))
+#define NT (256)
+#define NB (N / NT)
+
+__constant__ float a[N], b[N];
+
+__global__ void init(float *c)
+{
+    int i = blockIdx.x * blockDim.x + threadIdx.x;
+    c[i] = 0.0f;
+}
+
+__global__ void add(float *c)
+{
+    int i = blockIdx.x * blockDim.x + threadIdx.x;
+    c[i] = a[i] + b[i];
+}
+
+int main(void)
+{
+    float *c;
+    float *host_a, *host_b;
+    int i;
+
+    host_a = (float *)malloc(Nbytes);
+    host_b = (float *)malloc(Nbytes);
+
+    cudaMalloc((void **)&c, Nbytes);
+
+    for (i = 0; i < N; i++)
+    {
+        host_a[i] = 1.0f;
+        host_b[i] = 2.0f;
+    }
+
+    cudaMemcpyToSymbol(a, host_b, Nbytes);
+    cudaMemcpyToSymbol(b, host_b, Nbytes);
+
+    init<<<NB, NT>>>(c);
+    add<<<NB, NT>>>(c);
+
+    return 0;
+}
+
